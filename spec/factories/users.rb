b75cc2a6e17710c029b83b23476d58f857c8921a
Adam Dill <altrux@gmail.com>
2017-10-26 16:24:06 -0600

    [USERS] basic devise setup and admin bool

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :user do
+    email { Faker::Internet.email }
+    password 'password123'
+    admin false
+    trait :admin do
+      admin true
+    end
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:user) { FactoryGirl.build(:user, email: 'test@example.com') }
+
+  describe 'fields' do
+    it { is_expected.to respond_to(:email) }
+    it { is_expected.to respond_to(:admin) }
+    it { is_expected.to respond_to(:encrypted_password) }
+    it { is_expected.to respond_to(:sign_in_count) }
+    it { is_expected.to respond_to(:current_sign_in_at) }
+    it { is_expected.to respond_to(:last_sign_in_at) }
+    it { is_expected.to respond_to(:current_sign_in_ip) }
+    it { is_expected.to respond_to(:last_sign_in_ip) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:email) }
+  end
+
+  describe '#email' do
+    it 'matches a test example' do
+      expect(user.email).to match 'test@example.com'
+    end
+  end
+
+  describe '#admin' do
+    let(:admin) { FactoryGirl.build(:user, :admin) }
+    it 'returns false for a normal user' do
+      expect(user.admin).to be false
+    end
+    it 'returns true for an admin' do
+      expect(admin.admin).to be true
+    end
+  end
+end
