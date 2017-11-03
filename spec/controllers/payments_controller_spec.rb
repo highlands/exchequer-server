c1643629f6d183b1220cba20c2d82ec3b3f34889
Franzé Jr <franzejr@gmail.com>
2017-11-02 22:33:23 -0300

    Handle authentication in the test

--- spec/controllers/payments_controller_spec.rb
+++ spec/controllers/payments_controller_spec.rb
@@ -1,17 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe PaymentsController, type: :controller do
-  describe 'GET add_payment_method' do
-    it 'adds a payment method' do
-      get :add_payment_method
-      expect(response).to have_http_status(:success)
-    end
-  end
+  context 'Authenticated' do
+    let!(:user) { FactoryGirl.create(:user) }
 
-  describe 'GET payment_method_added' do
-    it 'callbacks when adding a new payment method' do
-      get :payment_method_added
-      expect(response).to have_http_status(:success)
+    before do
+      allow(controller).to receive(:authenticate_user!).and_return(true)
+      allow(controller).to receive(:current_user).and_return(user)
+    end
+
+    describe 'GET add_payment_method' do
+      it 'adds a payment method' do
+        get :add_payment_method
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe 'GET payment_method_added' do
+      it 'callbacks when adding a new payment method' do
+        get :payment_method_added
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 end
