9910627971c1f58496bfca7a3526c9a6dff9cebf
Franzé Jr <franzejr@gmail.com>
2017-11-11 18:38:38 -0300

    Fix tests for payments controller on adding a new payment method

--- spec/controllers/payments_controller_spec.rb
+++ spec/controllers/payments_controller_spec.rb
@@ -17,9 +17,29 @@ RSpec.describe PaymentsController, type: :controller do
     end
 
     describe 'GET payment_method_added' do
-      it 'callbacks when adding a new payment method' do
-        get :added
-        expect(response).to have_http_status(:success)
+      context 'when there are no errors in the card addition' do
+        before do
+          allow(RedirectionManager).to receive(:path_for).and_return('/')
+        end
+
+        it 'redirects to where RedirectionManager.path_for says' do
+          get :added
+          expect(response).to have_http_status(302)
+          expect(response).to redirect_to('/')
+        end
+      end
+
+      context 'when there are errors in the card addition' do
+        before do
+          allow(RedirectionManager).to receive(:path_for).and_return('/')
+          allow(controller).to receive(:params).and_return(error: 'error')
+        end
+
+        it 'renders added page' do
+          get :added
+          expect(response).to have_http_status(200)
+          expect(response).to render_template('payments/added')
+        end
       end
     end
   end
