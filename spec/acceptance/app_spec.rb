86a5998a33f635e92b1cb1dfa3b1b4180d95b972
Franzé Jr <franzejr@gmail.com>
2017-11-14 14:43:22 -0300

    Fix rubocop offenses

--- spec/acceptance/app_spec.rb
+++ spec/acceptance/app_spec.rb
@@ -5,16 +5,16 @@ resource 'App' do
     get '/api/v1/app/me' do
       example 'Showing Current App' do
         do_request
-        status.should == 401
+        expect(status).to eq 401
       end
     end
   end
+
   context 'Authenticated' do
     let(:offer)
     get '/api/v1/app/me' do
       example 'Showing Current App' do
         do_request
-        status.should == 401
         expect(response.keys).to eq %w[id application completion_content sections questions]
         expect(status).to eq(401)
       end
