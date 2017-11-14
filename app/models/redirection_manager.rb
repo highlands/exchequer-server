38329f7a98141b7e67d992a208a1ae6473e9f0be
Franzé Jr <franzejr@gmail.com>
2017-11-14 11:48:12 -0300

    Add tests for redirection manager

--- app/models/redirection_manager.rb
+++ app/models/redirection_manager.rb
@@ -3,6 +3,8 @@ class RedirectionManager
     include Rails.application.routes.url_helpers
 
     def path_for(params_in_session)
+      params_in_session = params_in_session.with_indifferent_access
+
       controller = params_in_session['controller']
       new_checkout_path(offer_id: params_in_session['offer_id']) if controller == 'checkouts'
     end
