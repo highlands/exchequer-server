d693f219d1fa8fd639ecfb46d712c657b9293989
Franzé Jr <franzejr@gmail.com>
2017-11-14 15:07:29 -0300

    Add tests for app endpoint

--- spec/acceptance/app_spec.rb
+++ spec/acceptance/app_spec.rb
@@ -1,6 +1,11 @@
 require 'acceptance_helper'
 
 resource 'App' do
+  let(:authentication) { nil }
+
+  header 'Content-Type', 'application/json'
+  header 'Authentication', :authentication
+
   context 'Unauthenticated' do
     get '/api/v1/app/me' do
       example 'Showing Current App' do
@@ -11,12 +16,18 @@ resource 'App' do
   end
 
   context 'Authenticated' do
-    let(:offer)
+    before do
+      FactoryGirl.create(:api_key, auth_token: Faker::Crypto.sha1)
+    end
+
+    let(:authentication) { ApiKey.first.auth_token }
+
     get '/api/v1/app/me' do
       example 'Showing Current App' do
         do_request
-        expect(response.keys).to eq %w[id application completion_content sections questions]
-        expect(status).to eq(401)
+        response = JSON.parse(response_body)
+        expect(response.keys).to eq %w[name]
+        expect(status).to eq(200)
       end
     end
   end
--- spec/factories/api_keys.rb
+++ spec/factories/api_keys.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :api_key do
-    auth_token 'password'
+    auth_token Faker::Crypto.sha1
     app
   end
 end
