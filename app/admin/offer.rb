11a2b3c72edbbb5371458464f3922ab71bca77aa
Franzé Jr <franzejr@gmail.com>
2017-11-16 12:55:19 -0300

    Add searchable_select for active_admin and set up User,Offer and PaymentMethod on activeadmin

--- app/admin/offer.rb
+++ app/admin/offer.rb
@@ -1,4 +1,19 @@
 ActiveAdmin.register Offer do
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      f.input :manager,
+        as: :searchable_select,
+        collection: Manager.all,
+        member_label: proc { |manager| manager.name.to_s }
+      f.input :description
+      f.input :name
+      f.input :due_on
+      f.input :amount
+      f.input :deferrable
+    end
+    f.actions
+  end
   permit_params do
     %i[manager_id description name due_on amount deferrable]
   end
--- app/admin/payment_method.rb
+++ app/admin/payment_method.rb
@@ -1,4 +1,16 @@
 ActiveAdmin.register PaymentMethod do
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      f.input :token
+      f.input :user,
+        as: :searchable_select,
+        collection: User.all,
+        member_label: proc { |user| user.email.to_s }
+    end
+    f.actions
+  end
+
   permit_params do
     %i[user_id token]
   end
