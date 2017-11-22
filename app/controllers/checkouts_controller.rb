06c6cf524079d6b3ab3280eb661fe306ca7a54fd
Franzé Jr <franzejr@gmail.com>
2017-11-22 16:55:02 -0300

    Add payment button for offer. Paying the entire amount of an offer.

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -10,6 +10,40 @@ class CheckoutsController < ApplicationController
     @offer = Offer.find_by(id: params[:offer_id])
   end
 
+  def create
+    # For the payment token
+    # Should I get always the first payment token the user has?
+    payment_token = current_user.payment_methods.first.token
+    offer = Offer.find(params[:offer_id])
+    amount = params[:amount]
+
+    # We are retaining the payment method on success
+    # because this is not the default from spreedly
+    transaction = spreedly_env.purchase_on_gateway(Rails.application.secrets.gateway_token,
+      payment_token,
+      200,
+      retain_on_success: true)
+    if transaction.succeeded?
+      ActiveRecord::Base.transaction do
+        # It creates an invoice if there is not
+        # and after this it passes the invoice id
+        # to the payment
+        invoice = Invoice.find_or_create_by(offer: offer,
+                                            user: current_user,
+                                            due_on: offer.due_on || Time.zone.now)
+
+        # TODO: Handle LineItem
+
+        Payment.create(invoice: invoice, amount: amount)
+        flash[:success] = "You've just paid for this offer"
+      end
+    else
+      flash[:error] = 'Something went wrong.'
+    end
+
+    redirect_to new_checkout_path(offer_id: offer)
+  end
+
   private
 
   def check_path
