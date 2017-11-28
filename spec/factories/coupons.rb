441f1d4fcb0ade9e0869a53b6724be1a34c985a6
Franzé Jr <franzejr@gmail.com>
2017-11-27 22:47:18 -0300

    Add code to Coupons factory

--- spec/factories/coupons.rb
+++ spec/factories/coupons.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :coupon do
     offer
     name { Faker::Lorem.word }
+    code { Faker::Lorem.word }
     percent_off BigDecimal.new('10.00')
     trait :amount do
       percent_off nil
