9f272affda2f320a8aed1953b0bfc7020a9e02b0
Franzé Jr <franzejr@gmail.com>
2017-11-27 22:16:41 -0300

    Handle errors when a transaction does not succeds and add coupon field

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -17,29 +17,33 @@ class CheckoutsController < ApplicationController
     payment_token = current_user.payment_methods.first.token
     offer = Offer.find(params[:offer_id])
     amount = params[:amount]
+    coupon_name = params[:coupon]
+    coupon = Coupon.find_by(name: coupon_name, offer: offer)
 
-    # We are retaining the payment method on success
-    # because this is not the default from spreedly
-    transaction = spreedly_env.purchase_on_gateway(Rails.application.secrets.gateway_token,
-      payment_token,
-      amount,
-      retain_on_success: true)
-    if transaction.succeeded?
-      ActiveRecord::Base.transaction do
-        # It creates an invoice if there is not
-        # and after this it passes the invoice id
-        # to the payment
-        invoice = Invoice.find_or_create_by(offer: offer,
-                                            user: current_user,
-                                            due_on: offer.due_on || Time.zone.now)
+    begin
+      # We are retaining the payment method on success
+      # because this is not the default from spreedly
+      transaction = spreedly_env.purchase_on_gateway(Rails.application.secrets.gateway_token,
+        payment_token,
+        amount,
+        retain_on_success: true)
+      if transaction.succeeded?
+        ActiveRecord::Base.transaction do
+          # It creates an invoice if there is not
+          # and after this it passes the invoice id
+          # to the payment
+          invoice = Invoice.find_or_create_by(offer: offer,
+                                              user: current_user,
+                                              due_on: offer.due_on || Time.zone.now)
 
-        LineItem.create(invoice: invoice, offer: offer, quantity: 1, amount: amount)
+          LineItem.create(invoice: invoice, offer: offer, quantity: 1, amount: amount) if coupon
 
-        Payment.create(invoice: invoice, amount: amount, transaction_token: transaction.token)
-        flash[:success] = "You've just paid for this offer"
+          Payment.create(invoice: invoice, amount: amount, transaction_token: transaction.token)
+          flash[:success] = "You've just paid for this offer"
+        end
       end
-    else
-      flash[:error] = 'Something went wrong.'
+    rescue Spreedly::TransactionCreationError => e
+      flash[:error] = e.message
     end
 
     redirect_to new_checkout_path(offer_id: offer)
