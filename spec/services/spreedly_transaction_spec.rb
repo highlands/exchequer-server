f91958629f7370270156e67a9529621fda4bec30
Franzé Jr <franzejr@gmail.com>
2017-11-29 21:21:55 -0300

    Add spreedly transaction test

---
+++ spec/services/spreedly_transaction_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe SpreedlyTransaction do
+  describe '.purchase' do
+    context 'when transaction succeeds' do
+      let(:invoice) { double('Invoice') }
+      let(:transaction) { double('Transaction') }
+
+      before do
+        allow(SpreedlyTransaction)
+          .to receive_message_chain(
+            :spreedly_env, :purchase_on_gateway
+          ).and_return(transaction)
+        allow(transaction).to receive('succeeded?').and_return(true)
+        allow(transaction).to receive(:token).and_return('token')
+      end
+
+      it 'creates a payment' do
+        expect(Payment).to receive(:create).once
+        SpreedlyTransaction.purchase(invoice, 100, 'token')
+      end
+    end
+
+    context 'when transaction does not succeed' do
+      let(:invoice) { double('Invoice') }
+      let(:transaction) { double('Transaction') }
+
+      before do
+        allow(SpreedlyTransaction)
+          .to receive_message_chain(
+            :spreedly_env, :purchase_on_gateway
+          ).and_return(transaction)
+        allow(transaction).to receive('succeeded?').and_return(false)
+        allow(transaction).to receive(:token).and_return('token')
+      end
+
+      it 'creates a payment' do
+        expect(Payment).not_to receive(:create)
+        SpreedlyTransaction.purchase(invoice, 100, 'token')
+      end
+    end
+  end
+
+  context 'when transaction raises an exception' do
+    pending 'test when transaction raises an exception'
+  end
+end
