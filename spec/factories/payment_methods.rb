f785e975d4513fbc4a567e4a154d7fb23c5b588e
Franzé Jr <franzejr@gmail.com>
2017-12-01 19:42:34 -0300

    Add fields for last_four_digits and card_type

--- spec/factories/payment_methods.rb
+++ spec/factories/payment_methods.rb
@@ -2,5 +2,7 @@ FactoryGirl.define do
   factory :payment_method do
     user
     token Faker::Crypto.sha1
+    card_type 'visa'
+    last_four_digits '1234'
   end
 end
--- spec/models/payment_method_spec.rb
+++ spec/models/payment_method_spec.rb
@@ -3,10 +3,14 @@ require 'rails_helper'
 RSpec.describe PaymentMethod, type: :model do
   describe 'fields' do
     it { is_expected.to respond_to(:token) }
+    it { is_expected.to respond_to(:card_type) }
+    it { is_expected.to respond_to(:last_four_digits) }
   end
 
   describe 'validations' do
     it { is_expected.to validate_uniqueness_of(:token) }
+    it { is_expected.to validate_presence_of(:card_type) }
+    it { is_expected.to validate_presence_of(:last_four_digits) }
   end
 
   describe 'relations' do
