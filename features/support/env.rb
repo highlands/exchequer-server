dda462f32170052fd7fe72a57c10fead53455fb0
Franzé Jr <franzejr@gmail.com>
2017-12-13 15:28:39 -0300

    Use chrome headless

--- features/support/env.rb
+++ features/support/env.rb
@@ -29,7 +29,17 @@ Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w[headless disable-gpu] }
+  )
+
+  Capybara::Selenium::Driver.new app,
+    browser: :chrome,
+    desired_capabilities: capabilities
+end
+
 Spinach.hooks.on_tag('javascript') do
-  ::Capybara.current_driver = :selenium
+  ::Capybara.current_driver = :headless_chrome
   Capybara.current_session.current_window.resize_to(1920, 1080)
 end
