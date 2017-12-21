21f527113bb7b5715093e62b5e0f282050008d82
Franzé Jr <franzejr@gmail.com>
2017-12-21 16:32:04 -0300

    Use RedirectionManager as a module

--- app/models/redirection_manager.rb
+++ app/models/redirection_manager.rb
@@ -1,18 +1,13 @@
-# FIXME: this should be a module and maybe in an app/services folder
-class RedirectionManager
-  class << self
-    # change these methods into def self.foo
-    include Rails.application.routes.url_helpers
+module RedirectionManager
+  def self.path_for(params_in_session)
+    url_helpers = Rails.application.routes.url_helpers
+    params_in_session = params_in_session.with_indifferent_access
 
-    def path_for(params_in_session)
-      params_in_session = params_in_session.with_indifferent_access
+    controller = params_in_session['controller']
+    id = params_in_session['id']
 
-      controller = params_in_session['controller']
-      id = params_in_session['id']
+    return url_helpers.invoice_path(id) if controller == 'invoices' && id
 
-      return invoice_path(id) if controller == 'invoices' && id
-
-      return offer_path(id) if controller == 'offers' && id
-    end
+    return url_helpers.offer_path(id) if controller == 'offers' && id
   end
 end
