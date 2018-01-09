6ec70c55abfc876f599b5e6b12baa38cb430e219
Franzé Jr <franzejr@gmail.com>
2018-01-09 10:46:48 -0200

    Make sure we have break lines between validatios, relationships on models

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,11 +1,13 @@
 class Manager < ApplicationRecord
   # A Manager is the parent of all other objects in the data model. All other data should be
   # segmented by manager.
+
+  NotInSession = Class.new(StandardError)
+
   validates :name, presence: true
   validates :public_token, presence: true
 
   has_many :api_keys, dependent: :destroy
   has_many :offers, dependent: :destroy
 
-  NotInSession = Class.new(StandardError)
 end
