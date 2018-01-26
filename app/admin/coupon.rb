c90c6b2ba04c308fa9afdcafd59695a7ff508898
Franzé Jr <franzejr@gmail.com>
2018-01-25 22:33:38 -0300

    Change percent off inputs to % not decimal

--- app/admin/coupon.rb
+++ app/admin/coupon.rb
@@ -1,4 +1,15 @@
 ActiveAdmin.register Coupon do
+  index do
+    column :offer
+    column :name
+    column :code
+    column :percent_off do |p|
+      "#{p.percent_off * 100} %" if p.percent_off
+    end
+    column :amount_off
+    actions
+  end
+
   form do |f|
     f.semantic_errors
     f.inputs do
@@ -8,10 +19,32 @@ ActiveAdmin.register Coupon do
         member_label: proc { |offer| offer.name.to_s }
       f.input :name
       f.input :code
-      f.input :percent_off
+      f.inputs 'Discount amount - pick only one'  do
+        f.input :percent_off, label: 'Percent Off (in %)', input_html: { value: f.object.percent_off * 100 }
+      end
       f.input :amount_off
     end
     f.actions
+  end
+
+  controller do
+    def create
+      percent = percent_in_decimal
+      Coupon.create(permitted_params[:coupon].merge!(percent_off: percent))
+      redirect_to collection_path
+    end
+
+    def update
+      percent = percent_in_decimal
+      resource.update(permitted_params[:coupon].merge!(percent_off: percent))
+      redirect_to collection_path
+    end
+
+    def percent_in_decimal
+      percent = permitted_params[:coupon][:percent_off].to_f
+      percent /= 100 if percent
+      percent
+    end
   end
 
   permit_params do
